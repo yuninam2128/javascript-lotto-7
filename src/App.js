1111dc59f39809c4fe75e7c49444362fa01326da
yuni <yuninam2128@gmail.com>
2025-02-21 19:44:25 +0900

    ✨ Feat:전처리 기능 추가
    당첨 번호를 쉼표 기준으로 구분하고, 발행한 로또 개수를 구하는 기능을 추가하였다.

--- src/App.js
+++ src/App.js
@@ -8,15 +8,17 @@ class App {
     //구입금액 입력 
     const inputMoney = await Console.readLineAsync('구입금액을 입력해 주세요.');
     const moneyNum = inputMoney/1000;
-    Console.print(lottoGame.creatLotto(moneyNum));
+    Console.print(`${moneyNum}개를 구매했습니다.`);
+    for (const lotto in lottoGame.creatLotto(moneyNum)) {
+      Console.print(lottoGame.creatLotto(moneyNum)[lotto]);
+    }
+
+    //당첨번호 입력
     const inputNumbers = await Console.readLineAsync('당첨 번호를 입력해 주세요.');
+    const userLotto = lottoGame.preprocessUserLotto(inputNumbers);
+    Console.print(userLotto);
+
     const inputBonus = await Console.readLineAsync('보너스 번호를 입력해 주세요.');
-
-    //출력 
-    Console.print(inputMoney);
-    Console.print(inputNumbers);
-    Console.print(inputBonus);
-
   }
 }
 
--- src/LottoGame.js
+++ src/LottoGame.js
@@ -3,16 +3,30 @@ import Lotto from "../src/Lotto.js";
 
 class LottoGame{
     constructor(){
-        this.lottoList = []
+        this.winningLottos = []
     }
 
     //로또 발행
     creatLotto(num){
         for(let i = 0; i < num; i++){
-            let numbers = Random.pickUniqueNumbersInRange(1, 45, 6);
-            this.lottoList.push(new Lotto(numbers).getNumbers());
+            let numbers = Random.pickUniqueNumbersInRange(1, 45, 6); //중복되지 않는 숫자 6개 
+            this.winningLottos.push(new Lotto(numbers).getNumbers());
         }
-        return this.lottoList
+        this.winningLottos.sort((a,b) => a - b);
+        return this.winningLottos
+    }
+    
+    // userLotto 전처리 
+    preprocessUserLotto(userNumbers) {
+        return userNumbers.split(",").map(num => num.trim()).map(num => Number(num));
+    }
+
+    //로또 당첨조건 검사
+    checkLottoWinning(userLotto, winningLottos, bonusNumber) {
+        
+        // 일치하는 번호 개수 구하기 
+
+        // 보너스 번호와 일치 여부 확인 
     }
 }
 
