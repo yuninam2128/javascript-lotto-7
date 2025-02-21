630ba05e8193b444619963124d0257778d7d4356
yuni <yuninam2128@gmail.com>
2025-02-21 17:17:02 +0900

    ✨ Feat:입출력 기능 추가
    로또 구입 금액, 당첨 번호, 보너스 번호를 입력할 수 있는 기능을 추가하였다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    //구입금액 입력 
+    const inputMoney = await Console.readLineAsync('구입금액을 입력해 주세요.');
+    const inputNumbers = await Console.readLineAsync('당첨 번호를 입력해 주세요.');
+    const inputBonus = await Console.readLineAsync('보너스 번호를 입력해 주세요.');
+
+    //출력 
+    Console.print(inputMoney);
+    Console.print(inputNumbers);
+    Console.print(inputBonus);
+
+  }
 }
 
 export default App;
